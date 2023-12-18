6f86c93bafb6cad47dd39c02af0c2a8137f9eb46
alexlampe14 <lampealex888@gmail.com>
2023-12-18 13:12:18 -0500

    move clock into footer maybe?

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Clock from "./Clock";
+import React, { useState, useEffect } from 'react';
 
 type Icon = {
   src: string;
@@ -8,11 +8,27 @@ type Icon = {
   height: number;
 };
 
+type DateOptions = {
+  weekday: 'long';
+  year: 'numeric';
+  month: 'short';
+  day: 'numeric';
+};
+
+type TimeOptions = {
+  hour: 'numeric';
+  minute: 'numeric';
+  second: 'numeric';
+  hour12: false;
+};
+
 type FooterProps = {
   // Add any props here if needed
 };
 
 const Footer: React.FC<FooterProps> = () => {
+  const [isClient, setIsClient] = useState(false)
+  
   const icons: Icon[] = [
     {
       src: "icons/nextjs.svg",
@@ -40,6 +56,54 @@ const Footer: React.FC<FooterProps> = () => {
     },
   ];
 
+  const [currentDate, setCurrentDate] = useState<string>(
+    new Date().toLocaleDateString('en-US', {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+    } as DateOptions)
+      .split(',')
+      .join('')
+  );
+
+  const [currentTime, setCurrentTime] = useState<string>(
+    new Date().toLocaleTimeString('en-US', {
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+      hour12: false,
+    } as TimeOptions)
+  );
+
+  useEffect(() => {
+    setIsClient(true);
+    const intervalId = setInterval(() => {
+      setCurrentDate(
+        new Date().toLocaleDateString('en-US', {
+          weekday: 'long',
+          year: 'numeric',
+          month: 'short',
+          day: 'numeric',
+        } as DateOptions)
+          .split(',')
+          .join('')
+      );
+      setCurrentTime(
+        new Date().toLocaleTimeString('en-US', {
+          hour: 'numeric',
+          minute: 'numeric',
+          second: 'numeric',
+          hour12: false,
+        } as TimeOptions)
+      );
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
   return (
     <footer className="text-white p-4">
       <ul className="flex items-center justify-between">
@@ -53,7 +117,10 @@ const Footer: React.FC<FooterProps> = () => {
           </a>
         </li>
         <li>
-          <Clock />
+        <div className="flex flex-col items-center justify-center">
+          <p>{currentTime}</p>
+          <p>{currentDate}</p>
+        </div>
         </li>
         <li className="flex flex-col items-center space-x-2">
           <p>Made with: </p>
